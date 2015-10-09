bf2537604fea674b65095bc17cdf6c4e02fdd259
Daniel Baeck <db@sharpsrc.net>
2015-10-09 13:20:43 +0200

    model factories for basic models

--- logme/database/factories/ModelFactory.php
+++ logme/database/factories/ModelFactory.php
@@ -15,7 +15,50 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'password' => bcrypt("test123"),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\ValueType::class, function (Faker\Generator $faker) {
+    return [
+        'key' => 'time',
+        'title' => 'Time',
+        'description' => 'were tracking time in minutes'
+    ];
+});
+
+$factory->define(App\Project::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->domainWord,
+        'description' => $faker->sentence(),
+        'value_type_id' => 1,
+        'user_id' => 1
+    ];
+});
+
+$factory->define(App\Activity::class, function (Faker\Generator $faker) {
+
+    $start = $faker->dateTimeThisMonth;
+    $value = $faker->numberBetween(20, 200);
+    $end = $start->add(new DateInterval('PT'.$value.'M'));
+
+    return [
+        'start' => $start,
+        'end' => $end,
+        'value' => $value,
+        'comment' => $faker->sentence(),
+        'exact' => false,
+        'value_type_id' => 1,
+        'user_id' => 1,
+        'project_id' => 1
+    ];
+});
+
+$factory->define(App\Tag::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' => $faker->word,
+        'user_id' => 1
     ];
 });
